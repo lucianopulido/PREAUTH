74e37447ae66985ff4217a528ff6db798c0242f6
lucianopulido <lucianopulido1996@gmail.com>
2022-02-08 20:14:52 -0300

    refactoring terminado

--- preauth/ejercicio 2 - Spaghetti code/app/app.ts
+++
@@ -1,8 +0,0 @@
-import {GildedRose, Item} from "./gilded-rose";
-
-let item : Item = new Item('Aged Brie',10,10);
-let items = [] as Array<Item>;
-items.push(item);
-let gildedRose : GildedRose = new GildedRose(items);
-items = gildedRose.updateQuality();
-console.log(items);
