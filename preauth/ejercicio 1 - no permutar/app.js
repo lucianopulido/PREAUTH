a1520091674025b0d92276d8974d2e57d8800a24
lucianopulido <lucianopulido1996@gmail.com>
2022-02-08 21:50:20 -0300

    se optimizo el algoritmo

--- preauth/ejercicio 1 - no permutar/app.js
+++ preauth/ejercicio 1 - no permutar/app.js
@@ -1,20 +1,15 @@
 const subConjunto = (matriz, numero) => {
-  let matrizSubconjunto = [];
-  let total = 0;
+  let arraySubconjunto = [];
 
   for (let i = 0; i < matriz.length; i++) {
-    for (let j = 0; j < matriz.length; j++) {
-      total += matriz[i] + matriz[j];
-      if (total === numero) {
-        matrizSubconjunto.push(matriz[i]);
-        matrizSubconjunto.push(matriz[j]);
-        return matrizSubconjunto;
-      }
-      total = 0;
+    if (matriz[i] < numero && matriz.find((n) => n === numero - matriz[i])) {
+      arraySubconjunto.push(matriz[i]);
+      arraySubconjunto.push(numero - matriz[i]);      
+      return arraySubconjunto;
     }
   }
 };
 
-const matriz = [2, 5, 8, 14, 0];
+const matriz = [0, 2, 5, 8, 14];
 const numero = 10;
 console.log(subConjunto(matriz, numero));
